dbeba54e51b2616d4a0646bd32d13e94185c6547
Andrew M. Pankratz <apankra@wgu.edu>
2021-04-07 11:04:01 -0400

    Middleware tests

---
+++ tests/middleware/auth.test.js
@@ -0,0 +1,133 @@
+/* eslint-disable no-undef */
+/**
+ * Auth middleware
+ * 
+ * @group middleware
+ * @group integration
+ */
+
+require('dotenv').config();
+const { permitRoles } = require('../../middleware/auth');
+const httpMocks = require('node-mocks-http');
+const { Auth, signToken } = require('../../_helpers/auth');
+
+describe(`permit roles middleware test`, () => {
+  it(`should send http status of 401 without an authorization header`, () => {
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/people",
+      headers: {
+          authorization: ""
+      }
+    });
+    const response = httpMocks.createResponse();
+    permitRoles(Auth.ADMIN, Auth.CONTRIBUTOR)(request, response);
+    expect(response.statusCode).toBe(401);
+  })
+
+  it("should return http status of 403 if there is an invalid authorization header", ()=>{
+    const request = httpMocks.createRequest({
+        method: "GET",
+        url: "/people",
+        headers: {
+            authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFkbWluIiwiYXV0aCI6ImFkbWluIiwiaWF0IjoxNjE2MjQ3MzcyLCJleHAiOjE2MTYyNDc0MzJ9.gWI40ko0ZaRo70cyqIVXAJmUrXugYv2UqqRsV9TkVJk"
+        }
+      });
+    const response = httpMocks.createResponse();
+    permitRoles(Auth.ADMIN, Auth.CONTRIBUTOR)(request, response);
+    expect(response.statusCode).toBe(403);
+  })
+
+  it(`should return http status of 200 with valid authorization header as admin with permitted admin and contributor`, done => {
+    const token = signToken('admin', Auth.ADMIN, '60s');
+        const request = httpMocks.createRequest({
+            method: "GET",
+            url: "/people",
+            headers: {
+              authorization: `Bearer ${token}`
+            }
+          });
+        const response = httpMocks.createResponse();
+        permitRoles(Auth.ADMIN, Auth.CONTRIBUTOR)(request, response, () => { done() });
+        expect(response.statusCode).toBe(200);
+  })
+
+  it(`should return http status of 200 with valid authorization header as contributor with permitted admin and contributor`, done => {
+    const token = signToken('admin', Auth.CONTRIBUTOR, '60s');
+        const request = httpMocks.createRequest({
+            method: "GET",
+            url: "/people",
+            headers: {
+              authorization: `Bearer ${token}`
+            }
+          });
+        const response = httpMocks.createResponse();
+        permitRoles(Auth.ADMIN, Auth.CONTRIBUTOR)(request, response, () => { done() });
+        expect(response.statusCode).toBe(200);
+  })
+
+  it(`should return http status of 403 with valid authorization header as contributor with no permitted contributor`, () => {
+    const token = signToken('admin', Auth.CONTRIBUTOR, '60s');
+        const request = httpMocks.createRequest({
+            method: "GET",
+            url: "/people",
+            headers: {
+              authorization: `Bearer ${token}`
+            }
+          });
+        const response = httpMocks.createResponse();
+        permitRoles(Auth.ADMIN)(request, response);
+        expect(response.statusCode).toBe(403);
+  })
+
+  it(`should return http status of 403 with valid authorization header as admin with no permitted admin`, () => {
+    const token = signToken('admin', Auth.ADMIN, '60s');
+        const request = httpMocks.createRequest({
+            method: "GET",
+            url: "/people",
+            headers: {
+              authorization: `Bearer ${token}`
+            }
+          });
+        const response = httpMocks.createResponse();
+        permitRoles(Auth.CONTRIBUTOR)(request, response);
+        expect(response.statusCode).toBe(403);
+  })
+
+  it(`should return http status of 200 with valid authorization header as contributor with same id permission`, done => {
+    const token = signToken('contributor', Auth.CONTRIBUTOR, '60s');
+        const request = httpMocks.createRequest({
+            method: "GET",
+            url: "/people",
+            headers: {
+              authorization: `Bearer ${token}`
+            },
+            params: {
+              id: 'contributor'
+            }
+          });
+        const response = httpMocks.createResponse();
+        permitRoles(Auth.ADMIN, Auth.SAME_ID)(request, response, () => { done() });
+        expect(response.statusCode).toBe(200);
+  })
+
+  it(`should return http status of 403 with valid authorization header with different ids with same id permission`, done => {
+    const token = signToken('contributor', Auth.CONTRIBUTOR, '60s');
+        const request = httpMocks.createRequest({
+            method: "GET",
+            url: "/people",
+            headers: {
+              authorization: `Bearer ${token}`
+            },
+            params: {
+              id: 'notcontributor'
+            }
+          });
+        const response = httpMocks.createResponse();
+        permitRoles(Auth.ADMIN, Auth.SAME_ID)(request, response, () => { done() });
+        expect(response.statusCode).toBe(403);
+        done();
+  })
+  
+
+})
