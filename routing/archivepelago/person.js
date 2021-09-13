0f383d84893835a80543bb43b34341a9a0303290
Andrew M. Pankratz <apankra@wgu.edu>
2021-09-13 09:47:49 -0400

    Add 401 to person POST without authorization cookie

--- routing/archivepelago/person.js
+++ routing/archivepelago/person.js
@@ -2,28 +2,9 @@ const express = require('express');
 const router = express.Router();
 const uriConfig = require('../uriConfig');
 
-// const { permitRoles } = require("../../middleware/auth");
-// const { Auth } = require('../../_helpers/auth');
+const { permitRoles } = require("../../middleware/auth");
+const { Auth } = require('../../_helpers/auth');
 const { sendStatus405 } = require('../../middleware/errors');
-
-/**
- * @swagger
- * /person:
- *   post:
- *     summary: Method not allowed.
- *     tags:
- *       - Person
- *     responses:
- *       405:
- *         description: Method not allowed.
- *         headers:
- *           Allow:
- *             schema:
- *               type: string
- *               example: 'GET'
- *             description: The methods allowed at this endpoint.
- */
-router.post(uriConfig.person, sendStatus405('GET'));
 
 /**
  * @swagger
@@ -39,10 +20,10 @@ router.post(uriConfig.person, sendStatus405('GET'));
  *           Allow:
  *             schema:
  *               type: string
- *               example: 'GET'
+ *               example: 'GET, POST'
  *             description: The methods allowed at this endpoint.
  */
-router.delete(uriConfig.person, sendStatus405('GET'));
+router.delete(uriConfig.person, sendStatus405('GET, POST'));
 
 /**
  * @swagger
@@ -61,6 +42,9 @@ router.delete(uriConfig.person, sendStatus405('GET'));
  *               example: 'GET'
  *             description: The methods allowed at this endpoint.
  */
-router.put(uriConfig.person, sendStatus405('GET'));
+router.put(uriConfig.person, sendStatus405('GET, POST'));
+
+
+router.post(uriConfig.person, permitRoles(Auth.ADMIN, Auth.CONTRIBUTOR), ()=>{});
 
 module.exports = router;
--- tests/routes/archivepelago/person.test.js
+++ tests/routes/archivepelago/person.test.js
@@ -12,8 +12,8 @@ const supertest = require("supertest");
 const uriConfig = require('../../../routing/uriConfig');
 // const { RoutingError, FieldError, EscalationError } = require("../../../_helpers/errors");
 // const { Auth } = require("../../../_helpers/auth");
-// const faker = require('faker');
-// const archiveNeo4jUsers = require('../../../archive-neo4j/user');
+const faker = require('faker');
+const archiveNeo4jPerson = require('../../../archive-neo4j/person');
 
 beforeAll(async () => {
   await serverInit(false, true);
@@ -24,23 +24,12 @@ afterAll(async () => {
 });
 
 describe (`${uriConfig.api + uriConfig.person} Routes`, () => {
-  it(`should return http status of 405 with Allow header 'GET' on POST`, (done) => {
-    supertest(server).post(uriConfig.api + uriConfig.person)
-      .expect(405)
-      .then((response) => {
-        expect(response.headers.allow).toBe('GET');
-        done();
-      })
-      .catch((error) => {
-        done(error);
-      })
-  })
 
   it(`should return http status of 405 with Allow header 'GET' on DELETE`, (done) => {
     supertest(server).delete(uriConfig.api + uriConfig.person)
       .expect(405)
       .then((response) => {
-        expect(response.headers.allow).toBe('GET');
+        expect(response.headers.allow).toBe('GET, POST');
         done();
       })
       .catch((error) => {
@@ -52,7 +41,38 @@ describe (`${uriConfig.api + uriConfig.person} Routes`, () => {
     supertest(server).put(uriConfig.api + uriConfig.person)
       .expect(405)
       .then((response) => {
-        expect(response.headers.allow).toBe('GET');
+        expect(response.headers.allow).toBe('GET, POST');
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      })
+  })
+
+  // it(`should return http status of 200 with list of people on GET`, async (done) => {
+  //   let person;
+  //   try{
+  //     person = await archiveNeo4jPerson.createPerson(faker.name.lastName(), faker.name.firstName(), faker.name.middleName());
+  //   }catch(e){
+  //     console.log(e);
+  //   }
+
+  //   supertest(server).get(uriCOnfig.api + uriConfig.person)
+  //     .expect(200)
+  //     .then((response) => {
+  //       expect(Array.isArray(response.body)).toBeTruthy();
+  //       expect(response.body).toContainEqual(person);
+  //       done();
+  //     })
+  //     .catch((error) => {
+  //       done(error);
+  //     })
+  // })
+
+  it(`should return http status of 401 on POST without authorization cookie`, (done) => {
+    supertest(server).post(uriConfig.api + uriConfig.person)
+      .expect(401)
+      .then(() => {
         done();
       })
       .catch((error) => {
